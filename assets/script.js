fd577cf4d36f106dca5412e0817e98c34ed19852
Sage Lupi <Sage@Sages-MacBook-Pro.local>
2023-05-25 18:59:00 -0400

    Added CSS styles

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,21 @@
 //weather API Key
+//var APIKey = "6fec1c3703735d08a384263c74be880a";
+//var city1 = "New York";
+//var queryURL = "http://api.openweathermap.org/data//2.5/weather?q=" + city1 + "&appid=" + APIKey;
+//var newYorkURL = "https://api.openweathermap.org/data/2.5/forecast?lat=40.7128&lon=-74.0060&appid={APIKey}"
+
+
+//function displayWeather(city1) {
+  //  document.getElementById('newYorkURL').innerText = `Weather information for ${city1}`;
+ // }
+
+
 var APIKey = "6fec1c3703735d08a384263c74be880a";
-var city = "Miami";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var city1 = "New York";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city1 + "&appid=" + APIKey;
 
-var newYorkURL = "https://api.openweathermap.org/data/2.5/forecast?lat=40.7128&lon=-74.0060&appid={APIKey}"
+function displayWeather(city1) {
+  document.getElementById('weather-info').innerText = `Weather information for ${city1}`;
+}
 
-console.log(newYorkURL);
+console.log(displayWeather);
