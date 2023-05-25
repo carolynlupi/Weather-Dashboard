fd9e804a58bfdd096b93897520e9eefe3c8b04ad
Sage Lupi <Sage@Sages-MacBook-Pro.local>
2023-05-25 15:07:43 -0400

    Added CSS styles to dashboard

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,4 @@
 //weather API Key
 var APIKey = "6fec1c3703735d08a384263c74be880a";
+var city = "Miami";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
