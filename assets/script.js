db1e14da2c7d33b15f577a0f46f0d9f4daab60e3
Sage Lupi <Sage@Sages-MacBook-Pro.local>
2023-05-25 15:40:59 -0400

    Added JS attributes

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,7 @@
 var APIKey = "6fec1c3703735d08a384263c74be880a";
 var city = "Miami";
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+var newYorkURL = "https://api.openweathermap.org/data/2.5/forecast?lat=40.7128&lon=-74.0060&appid={APIKey}"
+
+console.log(newYorkURL);
