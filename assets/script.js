84449565d7aec9268aa720ec50b8480f481fdd40
Sage Lupi <Sage@Sages-MacBook-Pro.local>
2023-05-27 21:17:06 -0400

    Add in jQuery

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,12 @@
-//weather API Key
-//var APIKey = "6fec1c3703735d08a384263c74be880a";
-//var city1 = "New York";
-//var queryURL = "http://api.openweathermap.org/data//2.5/weather?q=" + city1 + "&appid=" + APIKey;
-//var newYorkURL = "https://api.openweathermap.org/data/2.5/forecast?lat=40.7128&lon=-74.0060&appid={APIKey}"
-
-
-//function displayWeather(city1) {
-  //  document.getElementById('newYorkURL').innerText = `Weather information for ${city1}`;
- // }
-
-
+//all of my variables
 var APIKey = "6fec1c3703735d08a384263c74be880a";
-var city1 = "New York";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city1 + "&appid=" + APIKey;
+var city = "New York";
+var today = moment().format('L');
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-function displayWeather(city1) {
+function displayWeather(city) {
   document.getElementById('weather-info').innerText = `Weather information for ${city1}`;
 }
 
-console.log(displayWeather);
+
+console.log(today);
